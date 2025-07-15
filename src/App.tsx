bafde53b00c73a5f94880b4fbc3995c9c3a42d02
Ahmed Rabie <ahmedrabie@192.168.1.3>
2025-07-15 20:48:44 +0300

    Excersise showing and hiding alert     Using useState hook to manage state     and toggle alert visibility.     by passing a function to the Alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
+
+
 function App() {
 
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return <div className="container mt-5">
-    <Alert> Hello <span className="text-primary">My World</span>
-    </Alert>
-    <Button text="Click " onClick={() => console.log("Button Clicked!")} ButtonColor="primary" />
+
+    {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+
+    <Button text="My Button" onClick={() => setAlertVisibility(true)} ButtonColor="primary" />
   </div>;
 }
 
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from "react";
 
 interface Props {
     children: ReactNode;
+    onClose: () => void;
 }
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
     return (
-        <div className="alert alert-danger" role="alert">
-            {children}
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong> {children}
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onClose}  ></button>
         </div>
     )
 }
